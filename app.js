289d9f64b72acf89ba7571805ae43fc7986c4bd4
sujaltechno <151846684+merndeveloper0@users.noreply.github.com>
2026-04-13 17:50:51 +0530

    BASE_URL Change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "http://localhost:5000";
+const API = "https://tea-tracker-be.onrender.com";
 
 export default function App() {
   const [entries, setEntries] = useState([]);
